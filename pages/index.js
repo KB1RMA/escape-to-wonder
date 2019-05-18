d5e7114ddd4153ad569a9a1fe4313f12bbd33124
Christopher Snyder <cellulararrest@gmail.com>
2019-05-18 11:10:08 -0400

    Setup application with material-ui

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,10 @@
-export default () => <div className="test">Hello World. </div>;
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+export default () => (
+  <div className="test">
+    <Button variant="contained" color="primary">
+      Hello World.
+    </Button>
+  </div>
+);
