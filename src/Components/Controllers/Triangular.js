cff514882949f44dd2443b5150746baa4c08dd79
Christopher Snyder <cellulararrest@gmail.com>
2019-05-18 16:34:51 -0400

    First pass at a working concept of the triangular control mechanism

--- src/Components/Controllers/Triangular.js
+++ src/Components/Controllers/Triangular.js
@@ -5,25 +5,84 @@ import ReactCursorPosition from 'react-cursor-position';
 const ListenerWrapper = ({ imageIndex, dispatch }) => {
   const Listener = ({
     isActive,
-    position: { y },
-    elementDimensions: { height },
+    position: { x, y },
+    elementDimensions: { height, width },
   }) => {
-    const roundedYAxis = useMemo(() => y.toFixed(1), [y]);
+    const diagonalWidthContainer = useMemo(() => {
+      const a = 0 - width;
+      const b = 0 - height;
 
-    const opacity = useMemo(() => ((roundedYAxis / height) * 100).toFixed(1), [
+      const diagonalWidth = Math.sqrt(a * a + b * b).toFixed(0);
+      return diagonalWidth;
+    }, [height, width]);
+
+    // Opacity on first image is determined by plane of 0degrees (up and down, so we
+    //   can just figure out) the percentage y is of the height.
+    const opacity0 = useMemo(() => ((y / height) * 100).toFixed(1), [
       height,
-      roundedYAxis,
+      y,
     ]);
 
+    // Opacity on second image is on a plane 90 degrees across the square. So calculate
+    //   distance from the top-left of the rectance to the cursor.
+    const opacity1 = useMemo(() => {
+      const a = 0 - x;
+      const b = 0 - y;
+      const diagonalPos = Math.sqrt(a * a + b * b).toFixed(1);
+
+      // What percentage of the total distance is the current position?
+      return ((diagonalPos / diagonalWidthContainer) * 100).toFixed(1);
+    }, [x, y, diagonalWidthContainer]);
+
+    // Opacity on third image is on a plane -90 degrees across the square. So calculate
+    //   distance from the top-right of the rectangle to the cursor.
+    const opacity2 = useMemo(() => {
+      const a = width - x;
+      const b = height - y;
+      const diagonalPos = Math.sqrt(a * a + b * b).toFixed(1);
+
+      // What percentage of the total distance is the current position?
+      return ((diagonalPos / diagonalWidthContainer) * 100).toFixed(1);
+    }, [x, y, diagonalWidthContainer, width, height]);
+
+    // Change index 0 image
     useEffect(() => {
       if (!isActive) return undefined;
 
       dispatch({
         type: 'opacity',
-        index: imageIndex,
-        opacity,
+        index: 0,
+        opacity: opacity0,
       });
-    }, [isActive, opacity]);
+
+      return undefined;
+    }, [isActive, opacity0]);
+
+    // Change index 1 image
+    useEffect(() => {
+      if (!isActive) return undefined;
+
+      dispatch({
+        type: 'opacity',
+        index: 1,
+        opacity: opacity1,
+      });
+
+      return undefined;
+    }, [isActive, opacity1]);
+
+    // Change index 2 image
+    useEffect(() => {
+      if (!isActive) return undefined;
+
+      dispatch({
+        type: 'opacity',
+        index: 2,
+        opacity: opacity2,
+      });
+
+      return undefined;
+    }, [isActive, opacity2]);
 
     return null;
   };
